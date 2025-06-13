ad82407a564cbb50c439414a8b08ed4778687c6c
barnettrob <barnett.rob@gmail.com>
2025-06-13 11:20:50 -0400

    handle different size images - back to masonry for desktop view

--- app/components/portfolio/AllWork.tsx
+++ app/components/portfolio/AllWork.tsx
@@ -15,24 +15,37 @@ const AllWork = (props: PortfolioImagesProps) => {
     const images = props.images;
     
     return (
-        <div className='images-wrapper'>
+        <div className='masonry'>
             {images.map((post: any) => (
-                <div key={post.artworkImage.url} className='image-card' 
-                style={{ maxWidth: '380px', height: '300px', position: 'relative', overflow: 'hidden' }}>
+                <div key={post.artworkImage.url} className="artwork-card">
                     <Image
                         alt={post.artworkImage.title}
                         src={post.artworkImage.url}
                         quality={80}
-                        layout="fill"
-                        objectFit="cover"
-                        // width={post.artworkImage.width}
-                        // height={post.artworkImage.height}
-                        // style={{ maxWidth: '500px', height: 'auto' }}
+                        layout="responsive"
+                        width={post.artworkImage.width}
+                        height={post.artworkImage.height}
                         placeholder='blur'
                         blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjOHf4cAUAB4QCzf7jDSoAAAAASUVORK5CYII='
                         priority
                     />
                 </div>
+                // <div key={post.artworkImage.url} className='artwork-card' 
+                // style={{ height: '300px', position: 'relative' }}>
+                //     <Image
+                //         alt={post.artworkImage.title}
+                //         src={post.artworkImage.url}
+                //         quality={80}
+                //         layout="fill"
+                //         objectFit="cover"
+                //         // width={post.artworkImage.width}
+                //         // height={post.artworkImage.height}
+                //         // style={{ maxWidth: '500px', height: 'auto' }}
+                //         placeholder='blur'
+                //         blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjOHf4cAUAB4QCzf7jDSoAAAAASUVORK5CYII='
+                //         priority
+                //     />
+                // </div>
             ))}
         </div>
     )
