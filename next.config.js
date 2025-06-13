cb2a746ed74e7bbcca76f5afc8fffda5140fe739
barnettrob <barnett.rob@gmail.com>
2025-06-13 11:51:22 -0400

    allow nextjs to recognize contentful video cdn

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
             hostname: 'images.ctfassets.net',
             port: '',
             pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'video.ctfassets.net',
+            port: '',
+            pathname: '/**',
           }
         ],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
