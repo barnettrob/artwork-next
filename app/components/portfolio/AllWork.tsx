b38adb6e98dceeb225734b01384a553eae45682e
Robert Barnett <robertbarnett@MacBookAir.fios-router.home>
2025-10-20 13:59:58 -0400

    set video min width

--- app/components/portfolio/AllWork.tsx
+++ app/components/portfolio/AllWork.tsx
@@ -110,10 +110,10 @@ const AllWork = (props: PortfolioImagesProps) => {
                 let artwork = <></>
                 if (isVideo) {
                     const posterAttr = "videoImage" in post && post.videoImage !== null ? post.videoImage.url : "/video_poster_default.png";
-                    artwork = <video width={"100%"} height={"100%"} poster={posterAttr} controls>
+                    artwork = <div className="video-container"><video width={"100%"} height={"100%"} poster={posterAttr} controls>
                                 <source src={post.artworkImage.url} type="video/mp4" />
                                     Your browser does not support the video tag.
-                                </video>
+                                </video></div>
                 }
                 else {
                     artwork = <Link href={`?showModal=${showModalVal}`} className='artwork-link' onClick={(e) => handleOverlayImage(e, post)}>
