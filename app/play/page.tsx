1b5ba68904edb8d7f5271030793ecbc73c77bc13
Robert Barnett <robertbarnett@MacBookAir.fios-router.home>
2026-05-05 16:09:44 -0400

    fix typescript error

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,8 +1,9 @@
+import type { ComponentProps } from 'react';
 import PersonalWork from '../components/portfolio/PersonalWork';
 import AllWork from '../components/portfolio/AllWork'
 
 export default async function Home() {
-  const images = await PersonalWork();
+  const images = await PersonalWork() as unknown as ComponentProps<typeof AllWork>['images'];
 
   return (
     <div>
